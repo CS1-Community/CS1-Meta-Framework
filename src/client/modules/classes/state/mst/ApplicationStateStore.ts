c468ac8a072a5ea471ec23b991ce4c10fae7af91
Eric Eisaman <70238207+FrameBuffer4096@users.noreply.github.com>
2023-04-23 11:33:32 -0400

    moving ideation of AppStateStore forward

--- src/client/modules/classes/state/mst/ApplicationStateStore.ts
+++ src/client/modules/classes/state/mst/ApplicationStateStore.ts
@@ -9,30 +9,75 @@ import {
   getParent,
 } from "mobx-state-tree";
 
-const NetworkStateModel = types
-  .model("NetworkStateModel", {
+const LocalStateModel = types
+  .model("LocalStateModel", {
     ready: false,
   })
   .actions((scene) => ({
     setReady(cs1, resolve): void {
-      scene.ready = true;
-      console.log("SCENE READY!");
-      ApplicationStateStore.setReady(cs1, resolve);
+      BackendStateStore.setReady(cs1, resolve);
     },
   }));
 
-interface INetworkStateModel extends Instance<typeof NetworkStateModel> {}
-interface INetworkStateModelSnapshotIn
-  extends SnapshotIn<typeof NetworkStateModel> {}
-interface INetworkStateModelSnapshotOut
-  extends SnapshotOut<typeof NetworkStateModel> {}
+interface ILocalStateModel extends Instance<typeof LocalStateModel> {}
+interface ILocalStateModelSnapshotIn
+  extends SnapshotIn<typeof LocalStateModel> {}
+interface ILocalStateModelSnapshotOut
+  extends SnapshotOut<typeof LocalStateModel> {}
+
+export const LocalStateStore = LocalStateModel.create();
+
+const BackendStateModel = types
+  .model("BackendStateModel", {
+    ready: false,
+  })
+  .actions((scene) => ({
+    setReady(cs1, resolve): void {
+      // Await dynamic build out model based on API calls first.
+      PeersStateStore.setReady(cs1, resolve);
+    },
+  }));
+
+interface IBackendStateModel extends Instance<typeof BackendStateModel> {}
+interface IBackendStateModelSnapshotIn
+  extends SnapshotIn<typeof BackendStateModel> {}
+interface IBackendStateModelSnapshotOut
+  extends SnapshotOut<typeof BackendStateModel> {}
+
+export const BackendStateStore = BackendStateModel.create();
+
+const PeersStateModel = types
+.model("PeersStateModel", {
+  ready: false,
+})
+.actions((scene) => ({
+  setReady(cs1, resolve): void {
+    // Await signaling and webrtc related peer stuff first and
+    // build out model dynamically first.
+    ApplicationStateStore.setReady(cs1, resolve);
+  },
+}));
+
+interface IPeersStateModel extends Instance<typeof PeersStateModel> {}
+interface IPeersStateModelSnapshotIn
+extends SnapshotIn<typeof PeersStateModel> {}
+interface IPeersStateModelSnapshotOut
+extends SnapshotOut<typeof PeersStateModel> {}
+
+export const PeersStateStore = PeersStateModel.create();
 
 export const ApplicationStateModel = types
   .model("ApplicationStateModel", {
-    network: NetworkStateModel,
+    backend: BackendStateModel,
+    peers: PeersStateModel,
+    local: LocalStateModel,
     ready: false
   })
   .actions((application) => ({
+    prepare(cs1, resolve): void {
+      //DO API STUFF FIRST
+      BackendStateStore.setReady(cs1, resolve);
+    },
     setReady(cs1, resolve): void {
       application.ready = true;
       console.log("Application data store is READY!");
